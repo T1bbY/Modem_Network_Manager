c31f4d7dd73cf89f203344c1e632a5ba66979859
Thorsten Schwonbeck <ts@thermonova.dk>
2025-02-21 12:46:02 +0100

    Bugfix

--- src/utils/network.py
+++ src/utils/network.py
@@ -4,7 +4,7 @@ import json
 import os
 
 def load_config():
-    config_path = os.path.join('..', 'modem_config.json')
+    config_path = os.path.join('..', 'modem_config.cfg')
     with open(config_path, 'r') as file:
         config = json.load(file)
     return config
